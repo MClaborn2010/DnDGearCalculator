8e347f4302c61cab9ce0e1c815ac576a5d4a39c1
MCGaming1991 <mcgaming1991@gmail.com>
2024-04-15 17:57:50 -0500

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,180 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const itemTypeRarityValues = [
+  {
+    TwoHand: {
+      Junk: 15,
+      Poor: 22,
+      Common: 30,
+      Uncommon: 45,
+      Rare: 60,
+      Epic: 90,
+      Legendary: 120,
+      Unique: 175,
+    },
+  },
+  {
+    OneHandMain: {
+      Junk: 9,
+      Poor: 13,
+      Common: 18,
+      Uncommon: 27,
+      Rare: 36,
+      Epic: 54,
+      Legendary: 72,
+      Unique: 125,
+    },
+  },
+  {
+    OneHandOff: {
+      Junk: 7,
+      Poor: 10,
+      Common: 14,
+      Uncommon: 21,
+      Rare: 28,
+      Epic: 42,
+      Legendary: 56,
+      Unique: 100,
+    },
+  },
+  {
+    HandHeadFeet: {
+      Junk: 4,
+      Poor: 6,
+      Common: 8,
+      Uncommon: 12,
+      Rare: 16,
+      Epic: 24,
+      Legendary: 32,
+      Unique: 40,
+    },
+  },
+  {
+    ChestLegBack: {
+      Junk: 5,
+      Poor: 7,
+      Common: 10,
+      Uncommon: 15,
+      Rare: 20,
+      Epic: 30,
+      Legendary: 40,
+      Unique: 50,
+    },
+  },
+  {
+    Accessories: {
+      Uncommon: 9,
+      Rare: 12,
+      Epic: 18,
+      Legendary: 24,
+      Unique: 30,
+    },
+  },
+  {
+    Utilities: {
+      Junk: 2,
+      Poor: 3,
+      Common: 4,
+      Uncommon: 6,
+      Rare: 8,
+      Epic: 12,
+      Legendary: 16,
+      Unique: 20,
+    },
+  },
+];
 
 export default function Home() {
+  const [rarityQuantities, setRarityQuantities] = useState({});
+  const [totalScore, setTotalScore] = useState(0);
+
+  const handleQuantityChange = (itemType: any, rarity: any, quantity: any) => {
+    const updatedQuantities = {
+      ...rarityQuantities,
+      [itemType]: {
+        ...rarityQuantities[itemType],
+        [rarity]: quantity,
+      },
+    };
+    setRarityQuantities(updatedQuantities);
+  };
+
+  type ItemTypeRarityValues = {
+    [itemType: string]: { [rarity: string]: number };
+  };
+  type RarityQuantities = { [itemType: string]: { [rarity: string]: number } };
+
+  const calculateTotalScore = () => {
+    let totalScore: number = 0;
+
+    itemTypeRarityValues.forEach((itemTypeObj: ItemTypeRarityValues) => {
+      const itemType = Object.keys(itemTypeObj)[0];
+      const rarities = itemTypeObj[itemType];
+
+      let itemScore: number = 0;
+
+      Object.entries(rarities).forEach(([rarity, value]: [string, number]) => {
+        const quantity: number = rarityQuantities[itemType]?.[rarity] || 0;
+        itemScore += quantity * value;
+      });
+
+      totalScore += itemScore;
+    });
+
+    setTotalScore(totalScore); // Assuming setTotalScore is properly defined elsewhere
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="flex flex-col gap-2 items-center justify-center h-screen w-full">
+      <p>Dark and Darker Gear Calculator</p>
+      <div className="flex w-full items-center justify-center">
+        {itemTypeRarityValues.map((itemTypeObj, index) => {
+          const itemType = Object.keys(itemTypeObj)[0];
+          const rarities = itemTypeObj[itemType];
+
+          return (
+            <div key={index}>
+              <h2>{itemType}</h2>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Rarity</th>
+                    <th className="w-1/4">#</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Object.entries(rarities).map(([rarity, value]) => (
+                    <tr key={rarity}>
+                      <td>{rarity}</td>
+                      <td>
+                        <input
+                          className="w-1/2"
+                          type="number"
+                          min="0"
+                          value={rarityQuantities[itemType]?.[rarity] || 0}
+                          onChange={(e) =>
+                            handleQuantityChange(
+                              itemType,
+                              rarity,
+                              parseInt(e.target.value)
+                            )
+                          }
+                        />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          );
+        })}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+        <button onClick={calculateTotalScore}>Calculate Total Score</button>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+        <p className="text-8xl text-green-400">{totalScore}</p>
       </div>
     </main>
-  )
+  );
 }
